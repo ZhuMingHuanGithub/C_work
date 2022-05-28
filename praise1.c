d031ff9ff065820c78d65367dc17553aa408c878
朱明焕 <2306330018@qq.com>
2022-05-28 13:55:21 +0800

    Add files via upload

--- praise1.c
+++ praise1.c
@@ -6,7 +6,7 @@ int main(void)
     char name[40];
 
     printf("What's your name? ");
-    scanf("%s", name);
+    scanf("%s", &name);
     printf("Hello, %s. %s\n", name, PRAISE);
 
     getchar();
--- praise2.c
+++
@@ -1,4 +0,0 @@
-/* praise2.c */
-// 如果编辑器不识别 %zd, 尝试换成 %u 或 %lu。
-#include <stdio.h>
-#include <string.h>
