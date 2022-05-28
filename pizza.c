ffe4cc46598bb518ec3a89b3d067a0c47b8894b7
朱明焕 <2306330018@qq.com>
2022-05-28 19:58:39 +0800

    Update pizza.c

--- pizza.c
+++ pizza.c
@@ -4,4 +4,16 @@
 int main(void)
 {
     float area, circum, radius;
+
+    printf("What is the radius of your pizza?\n");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circum = 2.0 * PI * radius;
+    printf("Your basic pizza parameters are as follows:\n");
+    printf("circumference = %1.2f, area = %1.2f\n", circum, area);
+
+    getchar();
+    getchar();
+
+    return 0;
 }
