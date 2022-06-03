19c0ede453d156c392d091185b7394a0e4ecb1e7
ZhuMingHuan <2306330018@qq.com>
2022-06-03 17:02:07 +0800

    Create wheat.c

---
+++ wheat.c
@@ -0,0 +1,9 @@
+/* wheat.c -- 指数增长 */
+#include <stdio.h>
+#define SQUARES 64           // 棋盘中的方格数
+int main(void)
+{
+    const double CROP = 2E16;  // 世界小麦年产谷粒数
+    double current, total;
+    int count = 1;
+}
