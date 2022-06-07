820515654acc486baba59fc16503d312d27c308e
ZhuMingHuanGithub <2306330018@qq.com>
2022-06-07 09:41:30 +0800

    Merge branch 'main' of https://github.com/ZhuMingHuanGithub/C_work into main

--- addemup.c
+++ addemup.c
@@ -7,8 +7,22 @@ int main(void)
     count = 0;
     sum = 0;
     while (count++ < 20)
+<<<<<<< HEAD
     {
         sum = sum + count;
         printf("sum = %d\n", sum);
     }
 }
+=======
+        sum = sum + count;
+    printf("sum = %d\n", sum);
+
+    getchar();
+
+    return 0;
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 72156fbe085eb417450fab444975b6160feb2ca9
+>>>>>>> 63fc1ab7aab5de252f16fe9f9d982643891a70b8
