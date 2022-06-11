2b0bd35c0615826f21b3b7db0c7029adcf0fef6b
ZhuMingHuanGithub <2306330018@qq.com>
2022-06-11 17:20:20 +0800

    Create summing.c

--- summing.c
+++ summing.c
@@ -9,11 +9,7 @@ int main(void)
     printf("Please enter an integer to be summed ");
     printf("(q to quit): ");
     status = scanf("%ld", &num);
-<<<<<<< HEAD
     while (status == 1)    /* == 的意思是“等于”   */
-=======
-    while (status == 0)    /* == 的意思是“等于”   */
->>>>>>> 3011401af2b2f1c6ce941a8687a366f996114337
     {
         sum = sum + num;
         printf("Please enter next integer (q to quit): ");
