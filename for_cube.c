2688ae35701db7a3a174807e6bb6e0032e38bc6d
ZhuMingHuanGithub <2306330018@qq.com>
2022-06-15 16:10:12 +0800

    Update for_cube.c

--- for_cube.c
+++ for_cube.c
@@ -5,7 +5,7 @@ int main(void)
     int num;
 
     printf("   n   n cubed\n");
-    for(num = 1; num <= 6; num++)
+    for (num = 1; num <= 6; num++)
         printf("%5d %5d\n", num, num*num*num);
 
     getchar();
