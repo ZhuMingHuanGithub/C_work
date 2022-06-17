bbe759defe24ac5b687a2b7749e3558a7975fc68
ZhuMingHuanGithub <2306330018@qq.com>
2022-06-17 20:55:58 +0800

    Update entry.c

--- entry.c
+++ entry.c
@@ -8,7 +8,7 @@ int main(void)
     printf("To enter the triskaidekaphobia therapy club,\n");
     printf("please enter the secret code number: ");
     scanf("%d", &code_entered);
-    while (code_entered != secret_code);
+    while (code_entered != secret_code)
     {
         printf("To enter the triskaidekaphobia therapy club,\n");
         printf("please enter the secret code number: ");
