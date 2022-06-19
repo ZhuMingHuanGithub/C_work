b7aa20d4c73b15fb35bdcd19d6c2a45ae96ba52e
ZhuMingHuanGithub <2306330018@qq.com>
2022-06-19 18:27:09 +0800

    Update cypher2.c

--- cypher2.c
+++ cypher2.c
@@ -10,7 +10,8 @@ int main(void)
         if (isalpha(ch))        // 如果是一个字符，
             putchar(ch + 1);    // 显示该字符的下一个字符
         else                    // 否则，
-            putchar(ch);        // 原样显示g
+            putchar(ch);        // 原样显示
+    }
     putchar(ch);                // 显示换行符
 
     getchar();
