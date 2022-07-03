9370124f6e0424467094bc3aa93aa239877acdcf
ZhuMingHuanGithub <2306330018@qq.com>
2022-07-03 08:00:29 +0800

    Create checking.c

--- checking.c
+++ checking.c
@@ -10,10 +10,10 @@ bool bad_limits(long begin, long end,
 double sum_squares(long a, long b);
 int main(void)
 {
-    const long MIN = -10000000L;
-    const long MAX = 10000000L;
-    long start;
-    long stop;
+    const long MIN = -10000000L;   // 范围的下限
+    const long MAX = +10000000L;   // 范围的上限
+    long start;                    // 用户指定的范围最小值
+    long stop;                     // 用户指定的范围最大值
     double answer;
 
     printf("This program computes the sum of the squares of "
@@ -27,7 +27,7 @@ int main(void)
     stop = get_long();
     while (start != 0 || stop != 0)
     {
-        if (bad_limits(start, stop, MIN, MIN))
+        if (bad_limits(start, stop, MIN, MAX))
             printf("Please try again.\n");
         else
         {
@@ -65,4 +65,39 @@ long get_long(void)
     }
 
     return input;
+}
+
+double sum_squares(long a, long b)
+{
+    double total = 0;
+    long i;
+
+    for (i = a; i <= b; i++)
+        total += (double) i * (double) i;
+
+    return total;
+}
+
+bool bad_limits(long begin, long end,
+                long low, long high)
+{
+    bool not_good = false;
+
+    if (begin > end)
+    {
+        printf("%ld isn't smaller than %ld.\n", begin, end);
+        not_good = true;
+    }
+    if (begin < low || end < low)
+    {
+        printf("Values must be %ld or greater.\n", low);
+        not_good = true;
+    }
+    if (begin > high || end > high)
+    {
+        printf("Values must be %ld or less.\n", high);
+        not_good = true;
+    }
+
+    return not_good;
 }
